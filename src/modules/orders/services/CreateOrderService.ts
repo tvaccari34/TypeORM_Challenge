0a8345fd700c1bfe5674b7bb1504e1ee6a38f8ba
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-07-26 11:06:36 +0100

    Repositories and Services classes created

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,63 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrderRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomerRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer does not exist.');
+    }
+
+    const productList = await this.productsRepository.findAllById(products);
+
+    const quantityProductUpdate = products.map(product => {
+      const productFound = productList.find(p => p.id === product.id);
+
+      if (!productFound) {
+        throw new AppError('Product not found.');
+      }
+
+      const totalQuantity = productFound.quantity - product.quantity;
+
+      productFound.quantity = totalQuantity;
+
+      return productFound;
+    });
+
+    const productsFormatted = products.map(product => {
+      const productPrice = productList.find(p => p.id === product.id);
+
+      if (!productPrice) {
+        throw new AppError('Product not found.');
+      }
+
+      const productFormatted = {
+        product_id: product.id,
+        price: productPrice.price,
+        quantity: product.quantity,
+      };
+
+      return productFormatted;
+    });
+
+    await this.productsRepository.updateQuantity(quantityProductUpdate);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: productsFormatted,
+    });
+
+    return order;
   }
 }
 
--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -2,9 +2,20 @@ import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
+import CreateOrderService from '@modules/orders/services/CreateOrderService';
 
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, price, quantity } = request.body;
+
+    const createProduct = container.resolve(CreateProductService);
+
+    const product = await createProduct.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
   }
 }
