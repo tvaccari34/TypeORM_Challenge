0e16e7a8547a4d06a3e5aaecaba2bcf680a407c7
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-07-26 11:23:13 +0100

    Repositories and Services classes created

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,52 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const ids = products.map(product => {
+      const { id } = product;
+      return id;
+    });
+
+    const productList = await this.ormRepository.find({
+      where: { id: In(ids), },
+    });
+
+    return productList;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productList = await this.findAllById(products);
+
+    const updateProducts = productList.map(product => {
+      const tempProduct = new Product();
+
+      Object.assign(tempProduct, product);
+
+      const assignedProduct = products.find(p => p.id === product.id);
+      const quantity = assignedProduct ? assignedProduct.quantity : 0;
+      tempProduct.quantity = quantity;
+
+      return tempProduct;
+    });
+
+    const savedProduct = await this.ormRepository.save(updateProducts);
+
+    return savedProduct;
   }
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
+import ProductsRepository from '../infra/typeorm/repositories/ProductsRepository';
 
 interface IRequest {
   name: string;
@@ -13,10 +14,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject(ProductsRepository)
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const existProduct = await this.productsRepository.findByName(name);
+
+    if (existProduct) {
+      throw new AppError('Product not found.');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
